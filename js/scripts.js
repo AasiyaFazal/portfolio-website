26c0c7b6f408bb4449b39ad5302b84548865efd2
AasiyaFazal <aasiya.fazal@gmail.com>
2023-02-01 19:50:47 +0000

    Change to hero image

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,14 @@ function menuToggle() {
   checkbox.checked = null;
 
 }
+
+$(function() {
+$(window).on("scroll", function() {
+if($(window).scrollTop() > 50) {
+$(".header-home").addClass(" active-bg");
+} else {
+//remove the background property so it comes transparent again (defined in your css)
+$(".header-home").removeClass("active-bg");
+}
+});
+});

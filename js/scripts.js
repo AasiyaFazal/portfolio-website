0a26f345bb0740de13db93efe6021bde347a9979
AasiyaFazal <aasiya.fazal@gmail.com>
2023-02-03 18:24:43 +0000

    Changes to code for Accessibility and Quality

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,15 @@
 function menuToggle() {
-  var checkbox = document.getElementById('checkBox');
+  var checkbox = document.getElementById("checkBox");
   checkbox.checked = null;
-
 }
 
-$(function() {
-$(window).on("scroll", function() {
-if($(window).scrollTop() > 50) {
-$(".header-home").addClass(" active-bg");
-} else {
-//remove the background property so it comes transparent again (defined in your css)
-$(".header-home").removeClass("active-bg");
-}
-});
+$(function () {
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() > 50) {
+      $(".header-home").addClass(" active-bg");
+    } else {
+      //remove the background property so it comes transparent again (defined in your css)
+      $(".header-home").removeClass("active-bg");
+    }
+  });
 });

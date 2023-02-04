98a74cc0f5b8abf81866e776542461d9afb7f4be
AasiyaFazal <aasiya.fazal@gmail.com>
2023-02-04 20:29:21 +0000

    Updates to have modal image and change navigation bar colours

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,23 @@ $(function () {
     }
   });
 });
+
+// Modal Setup
+var modal = document.getElementById("modal");
+
+var modalClose = document.getElementById("modal-close");
+modalClose.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+
+// global handler
+document.addEventListener("click", function (e) {
+  if (e.target.className.indexOf("modal-image") !== -1) {
+    var img = e.target;
+    var modalImg = document.getElementById("modal-content");
+    var captionText = document.getElementById("modal-caption");
+    modal.style.display = "block";
+    modalImg.src = img.src;
+    captionText.innerHTML = img.alt;
+  }
+});
